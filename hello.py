48b92a0130f4aba9b16f52d1ccaed9c01a12a12b
Eric Dennison <ericd@netdenizen.com>
2013-07-20 16:33:56 -0400

    redis

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
 import os
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+
+def getredis():
+    redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+    return redis.from_url(redis_url)
 
 @app.route('/')
 def hello():
     return 'Hello World of Wonder and Delight!'
 
+@app.route('/set')
+def set():
+    r = getredis()
+    r.set('varname', 1001)
+    return 'We have set "varname" to 1001'
+
+@app.route('/get')
+def get():
+    r = getredis()
+    return str(r.get('varname'))
